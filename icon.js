d7b67c80002d036f799ed059bc244dfa52ff3abc
İsmail Caner <126804265+ismailcaner@users.noreply.github.com>
2024-07-31 12:58:31 +0300

    Create icon.js

---
+++ icon.js
@@ -0,0 +1,10 @@
+// PlusSignIcon.jsx
+import React from 'react';
+
+const PlusSignIcon = (props) => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width={48} height={48} color="#000000" fill="none" {...props}>
+    <path d="M12 4V20M20 12H4" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+  </svg>
+);
+
+export default PlusSignIcon;
